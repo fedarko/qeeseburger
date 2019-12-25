ee0bedbe3450b168913765746c59a29ea49fa069
Marcus Fedarko <mfedarko@umd.edu>
2019-12-24 19:54:48 -0800

    ENH: Add 'timestamp valid' col, err handling, etc.
    Various improvements, but this really underscores the need for adding
    unit tests to this code. Also I found myself reusing certain idioms
    a lot (e.g. parse(str(m_df.loc[sample_id, "collection_timestamp"])))
    -- would be worth abstracting some stuff to funcs, which could then
    be heavily unit tested

--- add_age_and_extra_times.py
+++ add_age_and_extra_times.py
@@ -45,8 +45,8 @@ def add_columns(
 ) -> None:
     """Add some useful columns for time-series studies to a metadata file.
 
-    In particular, the columns added are "host_age_years", "ordinal_timestamp",
-    and "days_since_first_day".
+    In particular, the columns added are "is_collection_timestamp_valid",
+    "host_age_years", "ordinal_timestamp", and "days_since_first_day".
     """
 
     host_birthday_datetime = parse(host_birthday)
@@ -61,7 +61,12 @@ def add_columns(
             "columns: {}".format(required_cols)
         )
 
-    output_cols = {"host_age_years", "ordinal_timestamp", "days_since_first_day"}
+    output_cols = {
+        "host_age_years",
+        "ordinal_timestamp",
+        "days_since_first_day",
+        "is_collection_timestamp_invalid",
+    }
     if len(output_cols & set(m_df.columns)) > 0:
         raise ValueError(
             "Input metadata file already includes at least one of the "
@@ -69,62 +74,93 @@ def add_columns(
         )
 
     m_df["host_age_years"] = 0
+    m_df["is_collection_timestamp_valid"] = "not applicable"
+
+    # Add on is_collection_timestamp_valid column
+    for sample_id in m_df.index:
+        sample_timestamp = m_df["collection_timestamp"][sample_id]
+        try:
+            sample_datetime = parse(str(sample_timestamp))
+            m_df.loc[sample_id, "is_collection_timestamp_valid"] = "True"
+        except (ValueError, TypeError):
+            m_df.loc[sample_id, "is_collection_timestamp_valid"] = "False"
+            print(
+                'WEIRD TIMESTAMP: "{}" for sample {}'.format(
+                    sample_timestamp, sample_id
+                )
+            )
 
     for sample_id in m_df.index:
         # We only compute age for samples with the specified host_subject_id
         if m_df.loc[sample_id, "host_subject_id"] == host_subject_id:
-            # Parse sample timestamp
-            sample_timestamp = m_df["collection_timestamp"][sample_id]
-            sample_datetime = parse(sample_timestamp)
+            if m_df.loc[sample_id, "is_collection_timestamp_valid"] == "True":
+                # Parse sample timestamp
+                sample_timestamp = m_df["collection_timestamp"][sample_id]
+                sample_datetime = parse(str(sample_timestamp))
 
-            rd = relativedelta(sample_datetime, host_birthday_datetime)
-            # Sanity check: the subject should never be negative years old
-            if rd.years > 0:
-                m_df.loc[sample_id, "host_age_years"] = str(rd.years)
-            else:
-                raise ValueError(
-                    "Sample {} has a collection_timestamp, {}, occurring before "
-                    "the host birthday of {}.".format(
-                        sample_id, sample_timestamp, host_birthday
+                rd = relativedelta(sample_datetime, host_birthday_datetime)
+                # Sanity check: the subject should never be negative years old
+                if rd.years > 0:
+                    m_df.loc[sample_id, "host_age_years"] = str(rd.years)
+                else:
+                    raise ValueError(
+                        (
+                            "Sample {} has a collection_timestamp, {}, "
+                            "occurring before the host birthday of {}."
+                        ).format(sample_id, sample_timestamp, host_birthday)
                     )
-                )
+            else:
+                m_df.loc[sample_id, "host_age_years"] = "not applicable"
         else:
             m_df.loc[sample_id, "host_age_years"] = "not applicable"
 
     # 2. Add ordinal timestamp for all samples
 
-    m_df["ordinal_timestamp"] = 0
+    m_df["ordinal_timestamp"] = "not applicable"
 
     for sample_id in m_df.index:
-        parsed_date = parse(m_df.loc[sample_id, "collection_timestamp"])
-        parsed_date_ordinalstring = parsed_date.isoformat()[:10].replace("-", "")
-        m_df.loc[sample_id, "ordinal_timestamp"] = parsed_date_ordinalstring
+        if m_df.loc[sample_id, "is_collection_timestamp_valid"] == "True":
+            parsed_date = parse(
+                str(m_df.loc[sample_id, "collection_timestamp"])
+            )
+            parsed_date_ordinalstring = parsed_date.isoformat()[:10].replace(
+                "-", ""
+            )
+            m_df.loc[
+                sample_id, "ordinal_timestamp"
+            ] = parsed_date_ordinalstring
 
     # 3. Add days elapsed
 
     # 3.1. Compute earliest date
     min_date = None
     for sample_id in m_df.index:
-        parsed_date = parse(m_df.loc[sample_id, "collection_timestamp"])
-        if min_date is None or parsed_date < min_date:
-            min_date = parsed_date
+        if m_df.loc[sample_id, "is_collection_timestamp_valid"] == "True":
+            parsed_date = parse(
+                str(m_df.loc[sample_id, "collection_timestamp"])
+            )
+            if min_date is None or parsed_date < min_date:
+                min_date = parsed_date
 
     print("Earliest date is {}.".format(min_date))
 
     # 3.2. Assign "days from first timestamp" metric for each sample
-    # (the sample(s) taken on min_date should have a value of 0, and samples taken
-    # exactly a day later would have a value of 1, ...)
-    # There is some inherent imprecision here due to different levels of precision
-    # in sample collection (e.g. down to the day vs. down to the minute), but this
-    # should be sufficient for exploratory visualization.
-    m_df["days_since_first_day"] = 0
+    # (the sample(s) taken on min_date should have a value of 0, and samples
+    # taken exactly a day later would have a value of 1, ...)
+    # There is some inherent imprecision here due to different levels of
+    # precision in sample collection (e.g. down to the day vs. down to the
+    # minute), but this should be sufficient for exploratory visualization.
+    m_df["days_since_first_day"] = "not applicable"
 
     for sample_id in m_df.index:
-        parsed_date = parse(m_df.loc[sample_id, "collection_timestamp"])
-        # Note the avoidance of relativedelta -- see
-        # https://stackoverflow.com/a/48262147/10730311
-        days_since = (parsed_date - min_date).days
-        m_df.loc[sample_id, "days_since_first_day"] = days_since
+        if m_df.loc[sample_id, "is_collection_timestamp_valid"] == "True":
+            parsed_date = parse(
+                str(m_df.loc[sample_id, "collection_timestamp"])
+            )
+            # Note the avoidance of relativedelta -- see
+            # https://stackoverflow.com/a/48262147/10730311
+            days_since = (parsed_date - min_date).days
+            m_df.loc[sample_id, "days_since_first_day"] = str(days_since)
 
     Metadata(m_df).save(output_metadata_file)
 
